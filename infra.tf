341526fb04a6495a0ae758703145d5ba0bc8198a
gamenithan <61070105@kmitl.ac.th>
2021-03-29 08:18:37 +0000

    first commit

---
+++ infra.tf
@@ -0,0 +1,50 @@
+{
+    "version": 3,
+    "serial": 1,
+    "lineage": "9b968d63-c28a-359f-cc93-4b38c25a6c8a",
+    "backend": {
+        "type": "s3",
+        "config": {
+            "access_key": null,
+            "acl": null,
+            "assume_role_policy": null,
+            "bucket": "lab9-61070105",
+            "dynamodb_endpoint": null,
+            "dynamodb_table": null,
+            "encrypt": true,
+            "endpoint": null,
+            "external_id": null,
+            "force_path_style": null,
+            "iam_endpoint": null,
+            "key": "infra-state",
+            "kms_key_id": null,
+            "lock_table": null,
+            "max_retries": null,
+            "profile": null,
+            "region": "us-east-1",
+            "role_arn": null,
+            "secret_key": null,
+            "session_name": null,
+            "shared_credentials_file": null,
+            "skip_credentials_validation": null,
+            "skip_get_ec2_platforms": null,
+            "skip_metadata_api_check": null,
+            "skip_region_validation": null,
+            "skip_requesting_account_id": null,
+            "sts_endpoint": null,
+            "token": null,
+            "workspace_key_prefix": null
+        },
+        "hash": 2089007522
+    },
+    "modules": [
+        {
+            "path": [
+                "root"
+            ],
+            "outputs": {},
+            "resources": {},
+            "depends_on": []
+        }
+    ]
+}
